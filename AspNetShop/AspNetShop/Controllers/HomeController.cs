85e0bdc2c999b9e46bae7dcc7eaf97becd6c3bb4
Volodymyr Martyniuk <vmartyniuk@playtika.com>
2022-02-20 09:37:47 +0200

    home page added

---
+++ AspNetShop/AspNetShop/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using AspNetShop.Data.Interfaces;
+using AspNetShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAllCars carRepository;
+
+        public HomeController(IAllCars carRepository)
+        {
+            this.carRepository = carRepository;
+        }
+
+        public ViewResult Index()
+        {
+            var homeCars = new HomeViewModel
+            {
+                FavCars = carRepository.GetFavCars
+            };
+            return View(homeCars);
+        }
+    }
+}
---
+++ AspNetShop/AspNetShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using AspNetShop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<CarModel> FavCars { get; set; }
+    }
+}
